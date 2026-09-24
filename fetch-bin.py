cbcb662f2794f2e17b6c354c4da0bb80f4dd859a
arkadeep <arkadeep@Arkadeeps-MacBook-Air.local>
2026-09-25 00:33:37 +0530

    Try the split option for irqchip. likely wont work, then we will revert. whats the harm in trying?

--- fetch-bin.py
+++ fetch-bin.py
@@ -112,7 +112,7 @@ int main() {
     char *whpxPos = strstr(args, "accel=whpx");
     if (whpxPos && !strstr(args, "kernel-irqchip=off")) {
         size_t prefixLen = whpxPos - args;
-        snprintf(newCmd, newCmdLen, "\"%s\" %.*saccel=whpx,kernel-irqchip=off%s",
+        snprintf(newCmd, newCmdLen, "\"%s\" %.*saccel=whpx,kernel-irqchip=split%s",
                  realExe, (int)prefixLen, args, whpxPos + strlen("accel=whpx"));
     } else {
         snprintf(newCmd, newCmdLen, "\"%s\" %s", realExe, args);
